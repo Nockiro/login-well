d4b1ef3dde038101b2ebdbe04079fe4a074ae2c4
nockiro <robinfreund99@gmx.de>
2017-03-20 16:57:46 +0100

    - adjustment for git live-pulling

--- internal/updateGit.php
+++ internal/updateGit.php
@@ -1,5 +1,5 @@
 <?php
-echo shell_exec("git stash save --keep-index 2>&1");
+echo shell_exec("git stash save --include-untracked --keep-index 2>&1");
 echo shell_exec("git pull 2>&1");
 echo shell_exec("git stash drop 2>&1");
 ?>
