014b900bf3688f6e3d5f1af4b69a57215701f1c5
nockiro <robinfreund99@gmx.de>
2017-03-20 13:24:56 +0100

    - style change - added host to build info (to notice if you are on your local host or not)

--- content/content/overview.php
+++ content/content/overview.php
@@ -17,22 +17,24 @@ if (isset($_GET['msg'])) {
     <p>Last Logout-Picture: <img src="account/getCard.php?img=<?php echo ($lastcard != -1 ? $lastcard : "nocard"); ?>" width="76" height="105"> </p>
     <p>Would you like to <a href="logout.php">logout</a>?</p>
 <?php else : ?>
-    <div style="width:600px">
-
+    <div class="content">
         <form method="post" action="account/process_login.php" name = "login_form">
             <table>
-                    <td>
-                        <label for="pass_id">E-Mail</label>
-                        <input type="text" name="email" id="email" value="" /></td>
-                    <td>
-                        <label for="pass_id">Password</label>
-                        <input type="password" name="password" id="password" value="" /></td>
-                    <td>
-                        <input type="button" onclick="formhash(this.form, this.form.password);" value="Login" /></td>
+                <td>
+                    <label for="pass_id">E-Mail</label>
+                    <input type="text" name="email" id="email" value="" /></td>
+                <td>
+                    <label for="pass_id">Password</label>
+                    <input type="password" name="password" id="password" value="" /></td>
+                <td>
+                    <input type="button" onclick="formhash(this.form, this.form.password);" value="Login" /></td>
             </table>
         </form>
 
     </div>
-    <p>If you don't have a login, please <a href=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>?cp=register">register</a>.</p>
-    <p>There <?php echo ($usercount > 1 ? "are" : "is") ?> currently <?php echo $usercount . ($usercount > 1 ? " Users" : " User"); ?> registered.</p>	
+
+    <div class="content">
+        <p>If you don't have a login, please <a href=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>?cp=register">register</a>.</p>
+        <p style="font-size: larger;">There <?php echo ($usercount > 1 ? "are" : "is") ?> currently <?php echo $usercount . ($usercount > 1 ? " Users" : " User"); ?> registered.</p>	
+    </div>
 <?php endif; ?>
