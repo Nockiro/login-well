67dd13895abce3b2bdb29e4a2c1f70a635c96f45
nockiro <robinfreund99@gmx.de>
2017-03-20 20:42:43 +0100

    - added icons for favourite bar / shorcuts

--- content/header.php
+++ content/header.php
@@ -12,3 +12,15 @@ else if (file_exists(file_build_path("..", "core", "functions.php")))
 <link rel="stylesheet" href="style.css" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 <script type="text/JavaScript" src="js/navigation.js"></script> 
+
+<!-- favicons/homescreen icons -->
+<link rel="apple-touch-icon" sizes="180x180" href="/img/favico/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="/img/favico/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/img/favico/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/img/favico/manifest.json">
+<link rel="mask-icon" href="/img/favico/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="/img/favico/favicon.ico">
+<meta name="apple-mobile-web-app-title" content="LoginWell">
+<meta name="application-name" content="LoginWell">
+<meta name="msapplication-config" content="/img/bfavico/rowserconfig.xml">
+<meta name="theme-color" content="#ffffff">
