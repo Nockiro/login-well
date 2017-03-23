2e36c23f4734755d6e30f2941238766c2a1e1c5d
nockiro <robinfreund99@gmx.de>
2017-03-23 21:22:06 +0100

    - added link in footer to the current branch

--- internal/revInfo.php
+++ internal/revInfo.php
@@ -1,5 +1,5 @@
 <?php
-
-echo "<small>website build " . shell_exec("git log -1 --format=\"%ci (%ar) @ %h\"") . " from " . shell_exec("git rev-parse --abbrev-ref HEAD") . " on " . $_SERVER['HTTP_HOST']  . "</small>";
+$branch = shell_exec("git rev-parse --abbrev-ref HEAD");
+echo "<small>website build " . shell_exec("git log -1 --format=\"%ci (%ar) @ %h\"") . " from <a style=\"font-size:14px;\" href=\"https://github.com/nockiro/login-well/tree/$branch\">$branch</a> on " . $_SERVER['HTTP_HOST']  . "</small>";
 
 ?>
