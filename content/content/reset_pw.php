ac25d1f81ba0689d8df05a98509739b5a0688bd2
TheLie0 <daniel.mowitz@online.de>
2017-04-25 18:28:11 +0200

    Update reset_pw.php
    i think I fixed it this time

--- content/content/reset_pw.php
+++ content/content/reset_pw.php
@@ -45,11 +45,13 @@ if (isset($_GET['submit'])) {
 ?>
 <div class="content">
   Sup, do you <strong>really</strong> wanna reset you password? <br>
+    <form action="" method="get">
     <div>
             <label for="email">E-Mail</label>
             <input type="text" name="email" id="email" value="" />
     </div>
-  <input type="submit" name ="submit" value="send"/>
+    <input type="submit" name ="submit" value="send"/>
+    </form>
 </div>
 <div class="content">
     <p style="font-size: large">Return to the <a href="index.php?cp">main page</a>.</p>
