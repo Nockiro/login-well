ac240206e41d8887d6279ecb172aaeb8a6be63f7
TheLie0 <daniel.mowitz@online.de>
2017-04-25 17:09:18 +0200

    Create reset_pw.php

---
+++ content/content/reset_pw.php
@@ -0,0 +1,8 @@
+<?php
+if (!empty($error_msg)) {
+    echo $error_msg;
+}
+?>
+<div class="content">
+  Sup, do you <strong>really</strong> wanna reset you password?
+  <input type="button" value="Register" onclick="return regformhash(this.form, />
