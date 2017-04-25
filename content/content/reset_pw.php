90bc0759df50086727e06ff50628573397462f0d
TheLie0 <daniel.mowitz@online.de>
2017-04-25 17:19:50 +0200

    Update reset_pw.php

--- content/content/reset_pw.php
+++ content/content/reset_pw.php
@@ -5,5 +5,12 @@ if (!empty($error_msg)) {
 ?>
 <div class="content">
   Sup, do you <strong>really</strong> wanna reset you password? <br>
+    <div>
+            <label for="email">E-Mail</label>
+            <input type="text" name="email" id="email" value="" />
+    </div>
   <input type="button" value="Yup." onclick="return regformhash(this.form, /> <br>
+</div>
+<div class="content">
+    <p style="font-size: large">Return to the <a href="index.php?cp">main page</a>.</p>
 </div>
