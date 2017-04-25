427b1b601caa806cdaed42cf2eaa874dc8b0eae8
TheLie0 <daniel.mowitz@online.de>
2017-04-25 17:05:02 +0200

    Update overview.php
    - oops

--- content/content/overview.php
+++ content/content/overview.php
@@ -85,7 +85,7 @@ $usercount = get_usercount($mysqli);
     </div>
 
     <div class="content">
-        <p>If you don't have a login, please <a href=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>?cp=register">register</a>.<a href=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>?cp=register">Forgot your password?</a></p>
+        <p>If you don't have a login, please <a href=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>?cp=register">register</a>.<a href=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>?cp=reset_pw">Forgot your password?</a></p>
         <p style="font-size: larger;">There <?php echo ($usercount > 1 ? "are" : "is") ?> currently <?php echo $usercount . ($usercount > 1 ? " Users" : " User"); ?> registered.</p>	
     </div>
 <?php endif; ?>
