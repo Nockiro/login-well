f1b30e06c0962c10aae383d680bed2d80604478b
nockiro <robinfreund99@gmx.de>
2017-05-01 18:21:14 +0200

    - improved logout page

--- content/content/logout.php
+++ content/content/logout.php
@@ -1,17 +1,20 @@
 <div class="content">
     <h3>Logout-Karte</h3>
     <hr/>
-<?php
-if (!isset($_COOKIE["img"])) {
-    echo "<div class=\"warn\"> Logout-Bild aus Sicherheitsgründen verfallen. </div>";
-} else {
-    echo '<img src="account/getCard.php?img=' . (isset($_COOKIE["img"]) ? $_COOKIE["img"] : "nocard") . ' width="76" height="105"><br/>';
-}
+    <div class="content">
+        Bei Logout wird ein Bild gezeigt, das beim Login vom Nutzer geprüft werden kann um zu sehen, ob sich in der Zwischenzeit jemand eingeloggt hat. <br/>
+        Dies ist der Fall, wenn die Login-Karte nicht der Karte beim Logout entspricht.
+    </div> <br/>
+    <?php
+    if (!isset($_COOKIE["img"])) {
+        echo "<div class=\"content warn\"> Logout-Bild aus Sicherheitsgründen verfallen. </div>";
+    } else {
+        echo '<img src="account/getCard.php?img=' . (isset($_COOKIE["img"]) ? $_COOKIE["img"] : "nocard") . ' width="76" height="105"><br/>';
+    }
 
 // clear last image cookie for security reasons
-setcookie("img", "", time() - 3600);
-
-?>
-<br/>
-Get <a href="/index.php">back</a> to the main page!
+    setcookie("img", "", time() - 3600);
+    ?>
+    <br/>
+    Get <a href="/index.php">back</a> to the main page!
 </div>
