8b152d8b06683733f8a5ee1b39790ae84ba4d810
nockiro <robinfreund99@gmx.de>
2017-05-06 19:45:07 +0200

    # included night mode in pagevisit

--- visit.php
+++ visit.php
@@ -3,10 +3,13 @@ include_once 'core/functions.php';
 $pid = htmlspecialchars($_GET["pid"]);
 $url = getURLFromPID($mysqli, $pid);
 
+if (isset($_COOKIE["setting"]))
+    $nightmode = $_COOKIE["setting"]["nightmode"]; 
 ?>
+
 <meta charset="UTF-8">
 <title><?php echo CONST_PROJNAME ?></title>
-<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="<?php if ($nightmode) { echo "nm"; } ?>style.css" />
 
 <a style="font-size: 14px;" href="/">◄ Zurück zu LoginWell </a>
 <div class="content">
